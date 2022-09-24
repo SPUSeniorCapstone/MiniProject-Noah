053a382f9c57a1e790a73976d1c42642107e98cf
Noah Gallo <gallon1@spu.edu>
2022-09-24 15:36:37 -0700

    Updated Player Controller

--- MiniProject/Assets/Scripts/PlayerController.cs
+++ MiniProject/Assets/Scripts/PlayerController.cs
@@ -6,21 +6,38 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed = 1;
+    public float jumpHeight = 5;
     public float sprintModifier = 2;
 
     public Camera cam;
     public PlayerModel model;
     public Terrain terrain;
 
+    [SerializeField]
+    public bool useRigidbody
+    {
+        get { return _useRigidBody; }
+        set { _useRigidBody = value; model.GetComponent<Collider>().enabled = value; }
+    }
+    private bool _useRigidBody = true;
+
+    private float _stoppedTill = 0;
+
+
+    private float _jump = 0;
     void Start()
     {
-        if(cam == null)
+        if (cam == null)
         {
             cam = Camera.main;
         }
-        if(model == null)
+        if (model == null)
         {
             Debug.LogError("PlayerController: Missing Player Model");
+        }
+        else
+        {
+            model.controller = this;
         }
     }
 
@@ -42,56 +59,76 @@ public class PlayerController : MonoBehaviour
             speed = moveSpeed;
         }
 
-        Vector3 newPos = model.transform.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            model.animatorState = PlayerModel.AnimatorState.MELEE_ATTACK_1H;
+            _stoppedTill = Time.time + model.GetStateTransitionTime(PlayerModel.AnimatorState.MELEE_ATTACK_1H);
+        }
+        if (_stoppedTill > Time.time)
+        {
+            return;
+        }
+
+        Vector3 move = Vector3.zero;
+        if(Input.GetAxis("Jump") != 0 && model.GetComponent<CharacterController>().isGrounded)
+        {
+            _jump = Input.GetAxis("Jump") * jumpHeight;
+
+        }
+
+        move += Vector3.up * _jump * Time.deltaTime;
+        _jump = _jump - 0.1f;
+        if (_jump < 0)
+        {
+            _jump = 0;
+        }
+
+
         if (Input.GetAxis("Horizontal") != 0)
         {
             Vector3 moveDir = cam.transform.right;
             moveDir.y = 0;
+            moveDir = moveDir.normalized;
 
             float moveHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             Vector3 temp = model.transform.position + moveDir * moveHorizontal;
-            if (CheckAngle(temp))
-            {
-                newPos += moveDir * moveHorizontal;
-            }
+            move += moveDir * moveHorizontal;
         }
 
         if (Input.GetAxis("Vertical") != 0)
         {
             Vector3 moveDir = cam.transform.forward;
             moveDir.y = 0;
+            moveDir = moveDir.normalized;
 
             float moveVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
             Vector3 temp = model.transform.position + moveDir * moveVertical;
-            if (CheckAngle(temp))
-            {
-                newPos += moveDir * moveVertical;
-            }
+            move += moveDir * moveVertical;
+
         }
 
-        RotateTowardsMovement(newPos);
 
-        if (CheckAngle(newPos))
+        var h = move;
+        h.y = 0;
+        if (h.magnitude > 0.01 && model.GetComponent<CharacterController>().isGrounded)
         {
-            float height = terrain.SampleHeight(newPos);
-            newPos.y = height;
-
-            if(newPos != model.transform.position)
-            {
-                model.animator.Play("RunForward");
-            }else
-            {
-                model.animator.Play("Idle");
-            }
-            model.transform.position = newPos;
+            model.animatorState = PlayerModel.AnimatorState.RUN_FORWARD;
         }
+        else if(model.GetComponent<CharacterController>().isGrounded)
+        {
+            model.animatorState = PlayerModel.AnimatorState.IDLE;
+        }
+
+        RotateTowardsMovement(model.transform.position + move);
+
+        model.GetComponent<CharacterController>().Move(move);
     }
 
     bool CheckAngle(Vector3 newPos)
     {
         float height = terrain.SampleHeight(newPos);
         float slope = Mathf.Abs((height - model.transform.position.y) / Vector3.Distance(newPos, model.transform.position));
-        if(slope > 1)
+        if (slope > 1)
         {
             return false;
         }
@@ -101,7 +138,7 @@ public class PlayerController : MonoBehaviour
 
     void RotateTowardsMovement(Vector3 newPos)
     {
-        if(newPos == model.transform.position)
+        if (newPos.x == model.transform.position.x && newPos.z == model.transform.position.z)
         {
             newPos = cam.transform.position;
         }
--- MiniProject/Assets/Scripts/PlayerModel.cs
+++ MiniProject/Assets/Scripts/PlayerModel.cs
@@ -7,6 +7,23 @@ public class PlayerModel : MonoBehaviour
 
     public float modelHeight = 1;
     public Animator animator;
+    public PlayerController controller;
+
+    public AnimatorState animatorState
+    {
+        get { return GetState(); }
+        set { SetState(value); }
+    }
+
+    private AnimatorState _state;
+
+    private float _lockedTill;
+
+    public enum AnimatorState { 
+        IDLE = 0,
+        RUN_FORWARD = 1,
+        MELEE_ATTACK_1H = 2
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +35,36 @@ public class PlayerModel : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void SetState(AnimatorState s)
+    {
+        if(_lockedTill > Time.time) { return; }
+
+        _lockedTill = Time.time + GetStateTransitionTime(s);
+        animator.CrossFade(GetStateString(s), 0.01f, 0);
+    }
+
+    private AnimatorState GetState()
+    {
+        return _state;
+    }
+
+    private string GetStateString(AnimatorState s)
+    {
+        if (s == AnimatorState.IDLE) { return "Idle"; }
+        if (s == AnimatorState.RUN_FORWARD) { return "RunForward"; }
+        if (s == AnimatorState.MELEE_ATTACK_1H) { return "MeleeAttack_OneHanded"; }
+
+        return "tpose";
+    }
+
+    public float GetStateTransitionTime(AnimatorState s)
+    {
+        if (s == AnimatorState.IDLE) { return 0f; }
+        if (s == AnimatorState.RUN_FORWARD) { return 0f; }
+        if (s == AnimatorState.MELEE_ATTACK_1H) { return 0.9f; }
+
+        return 0f;
     }
 }
